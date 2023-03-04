9fc29fe1f27e3378a67f98e7022f4a59d23ec582
Michal Urbanski <michalurb8@gmail.com>
2023-03-04 22:01:39 +0100

    finally converges

--- cmaes.py
+++ cmaes.py
@@ -1,19 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation
 from typing import List, Tuple
 from collections import deque
 import signal
+import time
 
 ######################
-CRITERIA = [lambda x: np.dot(x,x), lambda x: np.dot(x-2, x+3)]
+CRITERIA = [lambda x: np.dot(x,x)] #, lambda x: np.dot(x-2, x+3)]
 ######################
 
 _MAXIMISE = False
-_EPS = 1e-50
 _POINT_MAX = 1e100
 
-_DELAY = 0.2
+_DELAY = 0.05
 
 infp = float('inf')
 infn = float('-inf')
@@ -56,10 +55,8 @@ class CMAES:
         self._budget = 1000
 
         # Initial point
-        self._old_mean = None
-        self._new_mean = None
-        # Step size
-        self._sigma = 1
+        self._mean_m = None
+        self._mean_s = None
 
         # Population size
         if lambda_arg == None:
@@ -74,8 +71,12 @@ class CMAES:
         # E||N(0, I)||
         self._chi = np.sqrt(self._N) * (1 - 1 / (4 * self._N) + 1 / (21 * self._N ** 2))
 
+        # noise intensity
+        self._EPS = 10 ** (-8)/ self._chi
+
         # Evolution paths
-        self._path_c = np.zeros(self._N)
+        self._path = np.zeros(self._N)
+        self._c = 4/(self._N + 4)
 
         # Store current generation number
         self._generation = 0
@@ -101,46 +102,47 @@ class CMAES:
             if self._visuals:
                 if not self._visuals_started:
                     self._visuals_started = True
-                    plt.subplots(1, 2)
-                self._draw()
+                    
+                    plt.rcParams["figure.figsize"] = (8,9)
+                    plt.rcParams['font.size'] = '22'
+                    # plt.tight_layout()
+                    title = "Iteracja " + str(self._generation) + ", \n"
+                    title += "Liczebność populacji: " + str(self._lambda) + ", \n"
+                    title += "Wymiarowość: " + str(self._N) + ", \n"
+
+                    self._fig, (self._ax1, self._ax2) = plt.subplots(1, 2)
+                    self._fig.suptitle(title)
+                    self._fig.subplots_adjust(top = 0.8, bottom = 0.1, left = 0.1, right = 0.99)
+
+                self._draw_features()
+                self._draw_values()
+                plt.pause(_DELAY)
             self._update()
             self._new_generation()
 
     def _update(self) -> None:
         assert len(self._populations) <= self._H, f"There should be no more than H populations saved in history"
         assert len(self._populations[0]) == self._mu, f"There should be exactly mu points in each saved population"
-        assert len(self._populations[0][0]) == self._N, f"Each point should have exatcly N dimensions"
+        assert len(self._populations[0][0]) == self._N, f"Each point should have exactly N dimensions"
         for point in self._populations[0]:
             assert np.all(
                 np.abs(point) < _POINT_MAX
             ), f"Absolute value of all generated points must be less than {_POINT_MAX} to avoid overflow errors."
 
         #### UPDATE TODO
-        self._generation += 1
-
-
-    
-    def _get_diffs(self) -> List[np.array]:
-        diffs = []
-        for _ in range(self._lambda):
-            history_index = np.random.randint(0, min(len(self._populations), self._H))
-            x1_index = np.random.randint(0, self._lambda)
-            x2_index = np.random.randint(0, self._lambda)
-
-            diff = self._populations[history_index][x1_index][0] - self._populations[history_index][x2_index][0]
-            diffs.append(diff)
-
-        return diffs
     
     def _init_first_population(self, loc: np.array = 0, scale: float = 1) -> None:
         self._last_population = []
         for _ in range(self._lambda):
             new = np.random.normal(loc = loc, scale = scale, size = self._N)
             value = self._evaluate(new)
-            self._last_population.append((new, value))
-        selected = np.array([x[0] for x in sorted(self._last_population, key=lambda x: x[1], reverse=_MAXIMISE)][:self._mu]) ### implements NSGA sorting
-        self._old_mean = np.mean([x[0] for x in self._last_population], axis=0)
-        self._new_mean = np.mean(selected, axis=0)
+            rank = None
+            self._last_population.append((new, value, rank))
+
+        selected = np.array([x[0] for x in sorted(self._last_population, key=lambda x: x[1], reverse=_MAXIMISE)][:self._mu]) ### implement NSGA sorting
+
+        self._mean_m = np.mean(np.array([x[0] for x in self._last_population]), axis=0)
+        self._mean_s = np.mean(selected, axis=0)
 
         self._populations.append(selected)
 
@@ -149,24 +151,38 @@ class CMAES:
         for _ in range(self._lambda):
             new = self._sample_solution()
             value = self._evaluate(new)
-            self._last_population.append((new, value))
+            rank = None
+            self._last_population.append((new, value, rank))
 
-        selected = np.array([x[0] for x in sorted(self._last_population, key=lambda x: x[1], reverse=_MAXIMISE)][:self._mu])
+        selected = np.array([x[0] for x in sorted(self._last_population, key=lambda x: x[1], reverse=_MAXIMISE)][:self._mu]) ### implement NSGA sorting
 
-        self._old_mean = self._new_mean
-        self._new_mean = np.mean(selected, axis=0)
+        self._mean_m = np.mean(np.array([x[0] for x in self._last_population]), axis=0)
+        self._mean_s = np.mean(selected, axis=0)
+
+        self._path = (1-self._c) * self._path + self._c * (self._mean_s - self._mean_m)
+        print(self._mean_s - self._mean_m)
 
         self._populations.appendleft(selected)
         if len(self._populations) > self._H:
             self._populations.pop()
 
+        self._generation += 1
+
     def _sample_solution(self) -> np.ndarray:
-        return self._new_mean.copy() + np.random.standard_normal(self._N)
+        history_index = np.random.randint(0, min(len(self._populations), self._H))
+        x1_index = np.random.randint(0, self._mu)
+        x2_index = np.random.randint(0, self._mu)
+
+        diff = self._populations[history_index][x1_index] - self._populations[history_index][x2_index]
+        diff *= self._F
+        diff += self._path * self._chi * np.random.normal()
+        return self._mean_s + self._F * diff + self._EPS * np.random.standard_normal(self._N)
 
     def mean_history(self) -> List[float]:
         return self._mean_history
 
     def _evaluate(self, x):
+        # WORKAROUND:
         if self._count_eval < self._budget:
             return CRITERIA[0](x)
         return self._worst_fitness
@@ -175,38 +191,56 @@ class CMAES:
         #     return np.array([f(x) for f in CRITERIA])
         # return np.array([self._worst_fitness for _ in CRITERIA])
 
-    def _draw(self, _ = None):
-        plt.cla()
-        plt.clf()
+    def _draw_features(self, _ = None):
+        self._ax1.clear()
+        self._ax1.grid()
+
+        self._ax1.axis('equal')
+
+        self._ax1.axvline(0, linewidth=4, c='black')
+        self._ax1.axhline(0, linewidth=4, c='black')
+        x1 = [point[0][-1] for point in self._last_population]
+        x2 = [point[0][-2] for point in self._last_population]
+        sizes = [50 - point[1] for point in self._last_population]
+        self._ax1.scatter(x1, x2, s=sizes)
+        x1 = [point[-1] for point in self._populations[0]]
+        x2 = [point[-2] for point in self._populations[0]]
+        sizes = [30 - point[1] for point in self._populations[0]]
+        self._ax1.scatter(x1, x2, s=sizes)
+        # self._ax1.scatter(self._mean_m[-1], self._mean_m[-2], s=100, c='black')
+        # self._ax1.scatter(self._mean_s[-1], self._mean_s[-2], s=100, c='green')
+        zoom_out = 5
+        max1 = zoom_out*max([abs(point[0][-1]) for point in self._last_population])
+        max2 = zoom_out*max([abs(point[0][-2]) for point in self._last_population])
+        self._ax1.set_xlim(-max1, max1)
+        self._ax1.set_ylim(-max2, max2)
+
         title = "Iteracja " + str(self._generation) + ", \n"
         title += "Liczebność populacji: " + str(self._lambda) + ", \n"
         title += "Wymiarowość: " + str(self._N) + ", \n"
-        # title += "Funkcja celu: " + str(self._fitness.__name__)
-        plt.rcParams["figure.figsize"] = (8,9)
-        plt.rcParams['font.size'] = '22'
-        plt.tight_layout()
-        plt.subplots_adjust(top = 0.8, bottom = 0.1, left = 0.1, right = 0.99)
-        plt.title(title)
+        self._fig.suptitle(title)
 
-        plt.axis('equal')
+    def _draw_values(self, _ = None):
+        self._ax2.clear()
+        self._ax2.grid()
 
-        plt.axvline(0, linewidth=4, c='black')
-        plt.axhline(0, linewidth=4, c='black')
+        self._ax2.axis('equal')
+
+        self._ax2.axvline(0, linewidth=4, c='black')
+        self._ax2.axhline(0, linewidth=4, c='black')
         x1 = [point[0][-1] for point in self._last_population]
         x2 = [point[0][-2] for point in self._last_population]
-        plt.scatter(x1, x2, s=50)
+        self._ax2.scatter(x1, x2, s=50)
         x1 = [point[-1] for point in self._populations[0]]
         x2 = [point[-2] for point in self._populations[0]]
-        plt.scatter(x1, x2, s=15)
-        plt.scatter(self._new_mean[-1], self._new_mean[-2], s=100, c='black')
-        plt.scatter(self._old_mean[-1], self._old_mean[-2], s=100, c='green')
-        plt.grid()
-        zoom_out = 1.3
+        self._ax2.scatter(x1, x2, s=15)
+        # self._ax2.scatter(self._mean_m[-1], self._mean_m[-2], s=100, c='black')
+        # self._ax2.scatter(self._mean_s[-1], self._mean_s[-2], s=100, c='green')
+        zoom_out = 5
         max1 = zoom_out*max([abs(point[0][-1]) for point in self._last_population])
         max2 = zoom_out*max([abs(point[0][-2]) for point in self._last_population])
-        plt.xlim(-max1, max1)
-        plt.ylim(-max2, max2)
-        plt.pause(_DELAY)
+        self._ax2.set_xlim(-max1, max1)
+        self._ax2.set_ylim(-max2, max2)
 
 if __name__ == "__main__":
     CMAES(None, 2, 2, None, None)
