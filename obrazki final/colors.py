8d709881921538b84a697f9f89ab74298a8a60fd
Michał Urbański <michalurb8@gmail.com>
2024-02-06 01:06:21 +0100

    Add Pareto front/set code and images

--- obrazki final/colors.py
+++ obrazki final/colors.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from random import shuffle
 from functions import criteria
 
 COLORS = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple', 'magenta']
@@ -31,7 +32,7 @@ class Population:
         plt.show()
 
         res = 101
-        # res = 5
+        res = 41
         scale = 0.9
         for y in np.linspace(scale*self.ymin, scale*self.ymax, res):
             for x in np.linspace(scale*self.xmin, scale*self.xmax, res):
@@ -45,6 +46,11 @@ class Population:
         print("Points generated")
         self.calcRank()
         print("Ranks calculated")
+        self.survival()
+        print("Points removed")
+        for i in range(5):
+            self.reproduce()
+            print(f"Points duplicated {i}")
         self.refresh()
         print("Points drawn")
 
@@ -64,9 +70,19 @@ class Population:
         for point in self.points:
             index = (point[2]-1)//divisor + 1
             color = COLORS[get_CI(index)]
-            # color = np.random.rand(3,)
-            self.axCoord.scatter(point[0][0], point[0][1], color=color, s=14)
-            self.axVal.scatter(point[1][0], point[1][1], color=color, s=15, zorder=100-2*point[2])
+            color = np.random.rand(3,)
+            color = 'black'
+            self.axCoord.scatter(point[0][0], point[0][1], color=color, s=10)
+            self.axVal.scatter(point[1][0], point[1][1], color=color, s=10, zorder=100-2*point[2])
+        
+        if True:
+            for i, (x, y) in enumerate([(0, 3),(3, 0),(-0.45, 2),(2, -0.45),(-1.3,-0.4),(-0.4, -1.3)]):
+                color = np.random.rand(3,)
+                f1 = criteria[0](x, y)
+                f2 = criteria[1](x, y)
+                self.axCoord.scatter(x, y, color=COLORS[i], s=200, zorder = 1000)
+                self.axVal.scatter(f1, f2, color=COLORS[i], s=200, zorder = 1000)
+
         
         self.axCoord.set_title('Przestrzeń decyzyjna', fontsize=22)
         self.axCoord.set_xlabel('$x_1$', fontsize = 22, rotation = 0, labelpad = 15)
@@ -92,6 +108,28 @@ class Population:
                     currentPoint[2]=currentRank
             currentRank += 1
 
+    def survival(self) -> None:
+        self.points = [point for point in self.points if point[2] == 0]
+    
+    def reproduce(self) -> None:
+        newPoints = []
+        for point in self.points:
+            newX = point[0][0] + 0.1 * np.random.normal()
+            newY = point[0][1] + 0.1 * np.random.normal()
+            crit = np.array([criterium(newX, newY) for criterium in criteria])
+            newPoint = [np.array([newX, newY]), crit, INF]
+            newPoints.append(newPoint)
+        for point in self.points:
+            point[2] = INF
+        self.points.extend(newPoints)
+        self.calcRank()
+        self.survival()
+    
+    def addPoint(self, newX, newY) -> None:
+        crit = np.array([criterium(newX, newY) for criterium in criteria])
+        newPoint = [np.array([newX, newY]), crit, INF]
+        self.points.append(newPoint)
+
     def _drawBG(self, res = 101):
         x_ax = np.linspace(self.xmin, self.xmax, res)
         y_ax = np.linspace(self.ymin, self.ymax, res)
@@ -103,7 +141,7 @@ class Population:
             z = z - np.amin(z)
             z = z / np.amax(z)
             z = 10 * z
-            self.axCoord.contour(xGrid, yGrid, z, levels=list(np.array(list(range(30)))/3), colors=['blue', 'red'][index%COLOR_NUM], alpha=0.4)
+            self.axCoord.contour(xGrid, yGrid, z, levels=list(np.array(list(range(30)))/3), colors=['blue', 'red'][index%COLOR_NUM], alpha=0.2, zorder = -1000)
 
 def isDominated(p1, p2):
     for v1, v2 in zip(p1[1], p2[1]):
@@ -113,4 +151,5 @@ def isDominated(p1, p2):
 
 if __name__ == "__main__":
     bounds = [-4, 4, -4, 4]
+    bounds = [-2, 3.5, -2, 3.5]
     pop = Population(*bounds)
